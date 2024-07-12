debee5d984c754bfe3ecb44250dbfd19957452b8
hetviparekh01 <110492506+hetviparekh01@users.noreply.github.com>
2024-07-12 19:24:27 +0530

    image preview update

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -7,41 +7,97 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  userData:any;
-  role:string=this.ls.getRole() as string;
-  name:string=this.ls.getName() as string
-  constructor(private ls:LocalstorageService,private route:Router,private userService:UserServiceService){}
+  userData: any;
+  role: string = this.ls.getRole() as string;
+  name: string = this.ls.getName() as string;
+  selectedFile!: File;
+  temporaryURL!:string;
+  constructor(
+    private ls: LocalstorageService,
+    private route: Router,
+    private userService: UserServiceService
+  ) {}
   ngOnInit() {
-    this.getParticualrUser()
+    this.getParticualrUser();
+     
+  }
+  onSelectedFile(event: any) {
+    this.selectedFile = event?.target.files[0];
+  }
+  // loadFile(profile: any) {
+  //  let reader=new FileReader();
+  //  console.log(profile);
+  //  reader.onload=()=>{
+  //   let imageUpload=profile.id;
+  //   imageUpload.src=reader.result
+  //  }
+  //   reader.readAsDataURL(profile);
+  // }
+  loadFile(event: any, previewImage: HTMLImageElement) {
+    this.onSelectedFile(event);
+    const reader = new FileReader();
+    reader.onload = () => {
+      previewImage.src = reader.result as string;
+      this.temporaryURL=previewImage.src
+    };
+    if (this.selectedFile) {
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+  resetPreviewImage() {
+     this.temporaryURL = `http://localhost:3000/public/${this.userData?.profileImage}`;
   }
 
-  getParticualrUser(){
+  UpdateProfile() {
+    const formData = new FormData();
+    if (this.selectedFile) {
+      formData.append('profileImage', this.selectedFile);
+    }
+    this.userService.updateParticularUser(formData).subscribe({
+      next: (response: any) => {
+        Swal.fire({
+          icon: 'success',
+          title: response.content,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        this.getParticualrUser();
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.content,
+        });
+      },
+    });
+  }
+  getParticualrUser() {
     this.userService.getParticularUser().subscribe({
-        next:(response)=>{
-          this.userData=response.content
-        },
-        error:(error)=>{
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: error.error.content,
-          })
-        }
-    })
+      next: (response) => {
+        this.userData = response.content;
+        this.temporaryURL = `http://localhost:3000/public/${this.userData?.profileImage}`;
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.error.content,
+        });
+      },
+    });
   }
-
   logOut() {
-    this.ls.clearLocalstorage()
-     this.route.navigate(['/auth/login'])
-     Swal.fire({
+    this.ls.clearLocalstorage();
+    this.route.navigate(['/auth/login']);
+    Swal.fire({
       icon: 'success',
-      title: "USER LOGGED OUT SUCCESSFULLY",
+      title: 'USER LOGGED OUT SUCCESSFULLY',
       showConfirmButton: false,
       timer: 1500,
     });
   }
-
 }
